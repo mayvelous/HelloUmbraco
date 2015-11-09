c096c193d186d8a7c7855d1f2281797e06b2b6d9
may <mayvelous@gmail.com>
2015-11-09 23:45:28 +1100

    Added test plugin surfaceCtrl

---
+++ UmbTest.Plugins/ContactFormSurfaceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace UmbTest.Plugins
+{
+    [PluginController("UmbTest.Plugins")]
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+    }
+}
